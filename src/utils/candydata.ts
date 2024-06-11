74f5237777fc8229deb9d3cf77d04a1ae6102b1a
Titiksha Yadav <154905897+titikshaha@users.noreply.github.com>
2024-06-12 00:15:13 +0530

    Add files via upload

---
+++ src/utils/candydata.ts
@@ -0,0 +1,16 @@
+import blueCandy from "../images/blue-candy.png";
+import greenCandy from '../images/green-candy.png';
+import orangeCandy from '../images/orange-candy.png';
+import purpleCandy from '../images/purple-candy.png';
+import redCandy from '../images/red-candy.png';
+import yellowCandy from '../images/yellow-candy.png';
+import blank from '../images/blank.png';
+
+export const candies = [
+    blueCandy,
+    orangeCandy,
+    purpleCandy,
+    redCandy,
+    yellowCandy,
+    greenCandy
+]
